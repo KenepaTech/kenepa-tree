bc4d7154ecacb5fc7d19118ae51efe523f4761de
naisuu <julius.cathalina@gmail.com>
2021-11-05 23:26:47 +0100

    Add sass loader

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
+    <Sidebar />
     <TreeBrowser v-bind:resource_url="'https://raw.githubusercontent.com/KenepaTech/kenepa-tree/main/data/kenepa_resources_nested.json'" />
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+
 import TreeBrowser from './components/TreeBrowser.vue';
+import Sidebar from './components/Sidebar.vue';
+
+Vue.use(Buefy)
 
 export default {
   name: 'App',
   components: {
     TreeBrowser,
+    Sidebar,
   }
 }
 </script>
